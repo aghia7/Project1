0adbecd130a26b33653952589e689be1a27b6866
askar.khaimuldin <askar.khaimuldin@astanait.edu.kz>
2020-10-08 08:54:07 +0600

    add isPrime function

--- Source.cpp
+++ Source.cpp
@@ -2,6 +2,15 @@
 #include <iomanip>
 using namespace std;
 
+bool isPrime(int n) {
+	for (int i = 2; i * i <= n; i++) {
+		if (n % i == 0)
+			return false;
+	}
+
+	return true;
+}
+
 int main() {
 	int x = 7, y = 10;
 	bool flag = x > 5 && (y < 9 || y == 10);
