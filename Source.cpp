d9d5ca1cc09b9e2b82717e1db66d47dc8a7a09a5
askar.khaimuldin <askar.khaimuldin@astanait.edu.kz>
2020-10-08 08:55:41 +0600

    add mySwap function

--- Source.cpp
+++ Source.cpp
@@ -2,6 +2,13 @@
 #include <iomanip>
 using namespace std;
 
+template <class T>
+void mySwap(T& a, T& b) {
+	T temp = a;
+	a = b;
+	b = temp;
+}
+
 int main() {
 	int x = 7, y = 10;
 	bool flag = x > 5 && (y < 9 || y == 10);
